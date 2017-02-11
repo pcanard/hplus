653f808461887235e085e37740c6de541cca4fcf
nbeydoon <nori@cubyn.com>
2017-02-11 19:02:29 +0100

    Mapper function

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { json, urlencoded } = require('body-parser');
 
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/hplus');
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-    console.log('connected to db')
-});
+db.once('open', () => console.log('connected to db'));
 
-const inputRouter = express.Router()
+const Input = mongoose.model('Input', new mongoose.Schema({}, { strict: false }));
+
+const inputRouter = express.Router();
 
 inputRouter.route('/inputs')
     .put((req, res, next) => {
         const result = parseInputs(req.body.input);
 
+        const inputs = new Input(result);
+
+        inputs.save((err) => {
+            if (err) {
+                console.log(' OH MY GOD');
+            } else {
+                console.log('OKKKKK');
+            }
+        });
         res.json(result);
     })
     .get((req, res, next) => {
@@ -37,32 +48,82 @@ const app = express()
         console.log('Server started');
     });
 
-function parseInputs(input = '') {
-    const chunks = input.split(' ');
-    const res = {};
+const IDENTIFIERS_MAPPING = {
+    '@': 'users',
+    '@@': 'actions',
+    '#': 'places',
+    '##': 'colors',
+    '%': 'descriptions'
+};
+const SPECIAL_IDENTIFIERS = orderKeys(Object.keys(IDENTIFIERS_MAPPING));
+console.log(SPECIAL_IDENTIFIERS);
 
-    chunks.forEach(chunks => {
-        switch (chunks.substring(0, 1)) {
-            case '@': {
-                const content = chunks.substring(1);
-                if (!res.peoples) {
-                    res.peoples = [content]
-                } else {
-                    res.peoples.push(content);
+function parseInputs(input = '') {
+    const x = splitter(input);
+    return mapper(x);
+}
+
+function orderKeys(keys) {
+    return keys.sort((a, b) => a.length < b.length);
+}
+
+function mapper(chunks) {
+    const dest = {};
+
+    chunks.forEach(chunk => {
+        for (const key of SPECIAL_IDENTIFIERS) {
+            if (chunk.startsWith(key)) {
+                if (!dest[IDENTIFIERS_MAPPING[key]]) {
+                    dest[IDENTIFIERS_MAPPING[key]] = [];
                 }
-                break;
-            }
-            case '#': {
-                const content = chunks.substring(1);
-                if (!res.places) {
-                    res.places = [content]
-                } else {
-                    res.places.push(content);
-                }
-                break;
+                dest[IDENTIFIERS_MAPPING[key]].push(filter(chunk));
+                break ;
             }
         }
     });
 
-    return res;
+    return dest;
 }
+
+function filter(chunk) {
+    const identifier = SPECIAL_IDENTIFIERS.find(identifier => chunk.startsWith(identifier));
+
+    return chunk.substr(identifier.length);
+}
+
+function splitter(input) {
+    const dest = [];
+
+    for (let i = 0, len = input.length; i < len; i++) {
+        if (i === 0 || (i > 0  && input[i - 1] === ' ')) {
+            const endInput = input.substr(i);
+
+            const stop = SPECIAL_IDENTIFIERS.some(identifier => {
+                return endInput.startsWith(identifier);
+            });
+
+            if (stop) {
+                dest.push(toNextSplit(endInput));
+            }
+        }
+    }
+    return dest;
+}
+
+function toNextSplit(input) {
+    for (let i = 0, len = input.length; i < len; i++) {
+        if (i > 0  && input[i - 1] === ' ') {
+            const endInput = input.substr(i);
+
+            const stop = SPECIAL_IDENTIFIERS.some(identifier => {
+                return endInput.startsWith(identifier);
+            });
+
+            if (stop) {
+                return input.substr(0, i -1);
+            }
+        }
+    }
+    return input;
+}
+
